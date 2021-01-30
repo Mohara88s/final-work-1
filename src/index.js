6005c4ac3eee97d7591c4a93a9cd5f017402d9df
Mohara88s <mohara88s@gmail.com>
2021-01-30 19:55:33 +0200

    mobile version

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 import "./sass/main.scss";
+
+(() => {
+  const refs = {
+    openMenuBtn: document.querySelector('[data-menu-open]'),
+    closeMenuBtn: document.querySelector('[data-menu-close]'),
+    nav: document.querySelector('[data-menu]'),
+  };
+
+  refs.openMenuBtn.addEventListener('click', toggleMenu);
+  refs.closeMenuBtn.addEventListener('click', toggleMenu);
+
+  function toggleMenu() {
+    document.body.classList.toggle("menu-open");
+    refs.nav.classList.toggle('menu-in');
+  }
+})();
